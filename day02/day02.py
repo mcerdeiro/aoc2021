d729ea9574f06a1eeeeacdfa403c358154b9e3b4
Mariano Cerdeiro <mcerdeiro@gmail.com>
2021-12-02 21:36:26 +0100

    day2

---
+++ day02/day02.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+lines = open("day02.dat", "r").read().splitlines()
+#lines = [int(x) for x in lines]
+
+def mulHorizontalAndDepthPosition(movements):
+  pos = (0, 0)
+  moveTypes = {"forward": (1,0), "down": (0, 1), "up": (0, -1)}
+  for move in movements:
+    dir, moves =  move.split(" ")
+    moves = int(moves)
+    pos = (pos[0] + moveTypes[dir][0] * moves, 
+           pos[1] + moveTypes[dir][1] * moves)
+
+  return pos[0] * pos[1]
+    
+def mulHorizontalAndDepthPositionWithAim(movements):
+  pos = (0, 0, 0)
+  moveTypes = {"forward": (1,0, 1), "down": (0, 1, 0), "up": (0, -1, 0)}
+  for move in movements:
+    dir, moves =  move.split(" ")
+    moves = int(moves)
+    pos = (pos[0] + moveTypes[dir][0] * moves, 
+           pos[1] + moveTypes[dir][1] * moves,
+           pos[2] + moveTypes[dir][2] * pos[1] * moves)
+
+  return pos[0] * pos[2]
+
+print("Part1: ", mulHorizontalAndDepthPosition(lines))
+print("Part2: ", mulHorizontalAndDepthPositionWithAim(lines))
+  
+
