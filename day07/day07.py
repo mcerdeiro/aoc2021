bff86d2592e2f89f1f35e35c3cbcbe0a4d6081b4
Mariano Cerdeiro <mcerdeiro@gmail.com>
2021-12-07 11:40:03 +0100

    add day7

---
+++ day07/day07.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import copy
+from typing import List
+
+lines = open("day07.dat", "r").read().splitlines()
+
+vals = [int(x) for x in lines[0].split(",")]
+
+def fuelCostPart2(val):
+  return int (val * (val + 1) * 0.5)
+
+def fuelCostPart1(val):
+  return val
+
+
+mini = min(vals)
+maxi = max(vals)
+
+minFuelPart1 = 10000000000000000
+minFuelPart2 = 10000000000000000
+
+for check in range(mini,maxi):
+  fuelPart1 = 0
+  fuelPart2 = 0
+  for val in vals:
+    fuelPart1 += fuelCostPart1(abs(check - val))
+    fuelPart2 += fuelCostPart2(abs(check - val))
+
+  minFuelPart1 = min(minFuelPart1, fuelPart1)
+  minFuelPart2 = min(minFuelPart2, fuelPart2)
+
+print("Part1:", minFuelPart1)
+print("Part2:", minFuelPart2)
