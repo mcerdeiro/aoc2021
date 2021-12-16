7daf271cbeb3ff478b009ea19505dd3094dd4e5e
Mariano Cerdeiro <mcerdeiro@gmail.com>
2021-12-16 10:11:59 +0100

    some improvements, still dirty

--- day16/day16.py
+++ day16/day16.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import copy
+from os import device_encoding
 from typing import DefaultDict, List
 import sys
 from collections import defaultdict
@@ -11,7 +12,7 @@ MAP = {0: "sum", 1: "product", 2: "min", 3: "max", 5: ">", 6: "<", 7: "=="}
 
 p1 = 0
 
-def adapt(line):
+def toBinaryString(line):
   tmp = ""
   for el in line:
     val = str(bin(int(el,16)))
@@ -20,137 +21,123 @@ def adapt(line):
       tmp += "0"
     tmp += str(val[2:])
   return tmp
-  
-  
+
+def readBits(packet, length):
+  return int(packet[0:length],2)
+
+def readVersion(packet):
+  return readBits(packet, 3)
+
+def readPackedIdType(packet):
+  return readBits(packet[3:], 3)
+
+def getVal(packet):
+  offset = 0
+  value = ""
+  cont = True
+  while cont:
+    cont = (packet[offset] == "1")
+    offset += 1
+    value += packet[offset:offset+4]
+    offset += 4
+  return offset, int(value,2)
+    
+
 def process(packet):
   global p1
-  pos = 0
-  version = int(packet[pos:pos+3],2)  
-  p1 += version
-  value =  0
+  offset = 0
   
-  pos += 3
-  packtype = int(packet[pos:pos+3],2)
-  pos +=3
-  if packtype == 4:
-    val = ""
-    cont = True
-    while cont:
-      cont = (packet[pos] == "1")
-      pos += 1
-      val += packet[pos:pos+4]
-      pos += 4
-    value = int(val,2)
-    return pos, value
+  VERSION = readVersion(packet)
+  p1 += VERSION
+  PTID = readPackedIdType(packet)
+  
+  offset += 6
+  RESULT =  0
+
+  if PTID == 4:
+    o, RESULT = getVal(packet[offset:])
+    offset += o
+    return offset, RESULT
   else:
-    
-    I = int(packet[pos:pos+1],2)
-    pos+=1
+    LENGTHTYPEID = packet[offset:offset+1] == "1"
+    offset+=1
     lenghtL = 11
-    if (I==0):
+    if (LENGTHTYPEID==0):
       lenghtL = 15
 
-    L = int(packet[pos:pos+lenghtL],2)
-    pos += lenghtL
+    L = readBits(packet[offset:], lenghtL)
+    offset += lenghtL
+    
+    
+    
     values = []
-    posstart = pos
+    posstart = offset
+    
+    
     while True:
-      p, v = process(packet[pos:])
-      pos += p
+      p, v = process(packet[offset:])
+      offset += p
       values.append(v)
-      #print("p1", p1)
-      #print("Current pos", pos)
-      if I == 1:
+      if LENGTHTYPEID == 1:
         if len(values) == L:
           break
-      if I == 0:
-        print("Check", pos-posstart, L)
-        if pos-posstart == L:
+      if LENGTHTYPEID == 0:
+        if offset-posstart == L:
            break
       
     
-    if packtype == 0:
-      value = sum(values)
-    elif packtype == 1:
-      #print("Values", values, "operation", MAP[packtype], packtype, "=", value, "lenght", L, "in", "count" if I == 1 else "bits")
-      #assert(len(values) == 2)
-      value = 1
+    if PTID == 0:
+      RESULT = sum(values)
+    elif PTID == 1:
+      RESULT = 1
       for v in values:
-        value *= v
-    elif packtype == 2:
-      value = min(values)
-    elif packtype == 3:
-      value = max(values)
-    elif packtype == 5:
+        RESULT *= v
+    elif PTID == 2:
+      RESULT = min(values)
+    elif PTID == 3:
+      RESULT = max(values)
+    elif PTID == 5:
       assert(len(values) == 2)
-      value = (values[0] > values[1])
-    elif packtype == 6:
+      RESULT = (values[0] > values[1])
+    elif PTID == 6:
       assert(len(values) == 2)
-      value = (values[0] < values[1])
-    elif packtype == 7:
+      RESULT = (values[0] < values[1])
+    elif PTID == 7:
       assert(len(values) == 2)
-      value = (values[0] == values[1])
+      RESULT = (values[0] == values[1])
     else:
       assert(0)
-      
-    print("Values", values, "operation", MAP[packtype], packtype, "=", value, "lenght", L, "in", "count" if I == 1 else "bits")
-    #assert(len(values) == 2)
     
-    return pos, value
+    return offset, RESULT
   
 
-p,v = process(adapt("D2FE28"))
+assert(toBinaryString("D2FE28")=="110100101111111000101000")
+assert(toBinaryString("38006F45291200")=="00111000000000000110111101000101001010010001001000000000")
+assert(toBinaryString("EE00D40C823060")=="11101110000000001101010000001100100000100011000001100000")
+
+p,v = process(toBinaryString("D2FE28"))
 assert(v == 2021)  
-p,v = process(adapt("38006F45291200"))
+p,v = process(toBinaryString("38006F45291200"))
 assert(v == True) 
-p,v = process(adapt("C200B40A82"))
+p,v = process(toBinaryString("C200B40A82"))
 assert(v == 3)
-p,v = process(adapt("04005AC33890"))
+p,v = process(toBinaryString("04005AC33890"))
 assert(v == 54)
-p,v = process(adapt("880086C3E88112"))
+p,v = process(toBinaryString("880086C3E88112"))
 assert(v == 7)
-p,v = process(adapt("CE00C43D881120"))
+p,v = process(toBinaryString("CE00C43D881120"))
 assert(v == 9)
-p,v = process(adapt("D8005AC2A8F0"))
+p,v = process(toBinaryString("D8005AC2A8F0"))
 assert(v == True)
-p,v = process(adapt("F600BC2D8F"))
+p,v = process(toBinaryString("F600BC2D8F"))
 assert(v == 0)
-p,v = process(adapt("9C005AC2F8F0"))
+p,v = process(toBinaryString("9C005AC2F8F0"))
 assert(v == 0)
-p,v = process(adapt("9C0141080250320F1802104A08"))
+p,v = process(toBinaryString("9C0141080250320F1802104A08"))
 assert(v == 1)
 
-
-print("Test ENDS")
-#p,v = process(adapt("C200B40A82"))
-#assert(v == 3)  
-# p,v = process(adapt("04005AC33890"))
-# assert(v == 54)
-# p,v = process(adapt("880086C3E88112"))
-# assert(v == 7)
-
-line = adapt(lines[0])
-print("line", line)
-print(process(line))
-exit()
-
-print(tmp)
-version = int(tmp[0:3],2)
-print("version", version)
-pid = int(tmp[3:6],2)
-print("pid", pid)
-
-I = int(tmp[6:7],2)
-print(I)
-
-cur = 7
-lenghtL = 11
-if (I==0):
-  lenghtL = 15
-
-L = int(tmp[cur:cur+lenghtL],2)
-print(L)
-
-cur += lenghtL
-
-  
+line = toBinaryString(lines[0])
+p1 = 0
+_, v = process(line)
+print("Part1:", p1)
+print("Part2:", v)
