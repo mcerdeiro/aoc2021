522cda4c3bbe1108bee8fd2456b5205f231aead9
Mariano Cerdeiro <mcerdeiro@gmail.com>
2021-12-19 22:02:49 +0100

    part 1

--- day19/day19.py
+++ day19/day19.py
@@ -11,7 +11,76 @@ lines = open(sys.argv[1] if len(sys.argv) > 1 else "day19.dat", "r").read().spli
 S = []
 nextScanner = None
 
-def rotations(bacon):
+def getrotations(bacon):
+  ret = []
+  for ax in [1, -1]:
+    for ay in [1, -1]:
+      for az in [1, -1]:
+        x = bacon[0] * ax
+        y = bacon[1] * ay
+        z = bacon[2] * az
+        
+        ret.append((x,y,z))
+    
+  ret2 = []
+  for r in ret:
+    for rot in [0, 1,2]:
+      x,y,z = r
+      if rot == 0:
+        ret2.append((x,y,z))
+      elif rot == 1:
+        ret2.append((y,z, x))
+      elif rot == 2:
+        ret2.append((z, x, y))
+
+  assert(len(set(ret2))==24)
+  return ret2
+
+#R = getrotations((1,2,3))
+#print(len(R), len(set(R)), R)
+
+        
+        
+
+def rotationangle(bacon, pitch, roll, yaw):
+  cosa = [1, 0, -1, 0][yaw] # cos 0, 90, 180, 270 = 1, 0, -1, 0
+  sina = [0, 1, 0, -1][yaw] # cos 0, 90, 180, 270 = 0, 1, 0, -1
+  
+  cosb = [1, 0, -1, 0][pitch] # cos 0, 90, 180, 270 = 1, 0, -1, 0
+  sinb = [0, 1, 0, -1][pitch] # cos 0, 90, 180, 270 = 0, 1, 0, -1
+  
+  cosc = [1, 0, -1, 0][roll] # cos 0, 90, 180, 270 = 1, 0, -1, 0
+  sinc = [0, 1, 0, -1][roll] # cos 0, 90, 180, 270 = 0, 1, 0, -1
+  
+  Axx = cosa*cosb
+  Axy = cosa*sinb*sinc - sina*cosc
+  Axz = cosa*sinb*cosc + sina*sinc
+
+  Ayx = sina*cosb
+  Ayy = sina*sinb*sinc + cosa*cosc
+  Ayz = sina*sinb*cosc - cosa*sinc
+
+  Azx = -sinb
+  Azy = cosb*sinc
+  Azz = cosb*cosc
+  
+  px,py,pz = bacon
+  x = Axx*px + Axy*py + Axz*pz
+  y = Ayx*px + Ayy*py + Ayz*pz
+  z = Azx*px + Azy*py + Azz*pz
+  
+  return (x,y,z)
+  
+def getAllRotations(bacon):
+  ret = []
+  for pitch in range(4):
+    for roll in range(4):
+      for yaw in range(4):
+        ret.append(rotationangle(bacon, pitch, roll, yaw))
+
+  return ret
+
+def rotationsNotUSe(bacon):
   XA = [(1, 1, 1, 1), (0, 0, 0, 0), (0, 0, 0, 0)]
   YA = [(0, 0, 0, 0), (1, 0, -1, 0), (0, -1, 0, 1)]
   ZA = [(0, 0, 0, 0), (0, 1, 0, -1), (1, 0, -1, 0)]
@@ -31,8 +100,8 @@ def rotations(bacon):
       x = XA[0][ya] * r[0] + XA[1][ya] * r[1] + XA[2][ya] * r[2]
       y = YA[0][ya] * r[0] + YA[1][ya] * r[1] + YA[2][ya] * r[2]
       z = ZA[0][ya] * r[0] + ZA[1][ya] * r[1] + ZA[2][ya] * r[2]
-      if (x,y,z) not in R2:
-        R2.append((x,y,z))
+      #if (x,y,z) not in R2:
+      R2.append((x,y,z))
 
   XA = [(1, 0, -1, 0), (0, -1, 0, 1), (0, 0, 0, 0)]
   YA = [(0, 1, 0, -1), (1, 0, -1, 0), (0, 0, 0, 0)]
@@ -43,8 +112,8 @@ def rotations(bacon):
       x = XA[0][za] * r[0] + XA[1][za] * r[1] + XA[2][za] * r[2]
       y = YA[0][za] * r[0] + YA[1][za] * r[1] + YA[2][za] * r[2]
       z = ZA[0][za] * r[0] + ZA[1][za] * r[1] + ZA[2][za] * r[2]
-      if (x,y,z) not in R3:
-        R3.append((x,y,z))
+      #if (x,y,z) not in R3:
+      R3.append((x,y,z))
         
   return R3
 
@@ -60,12 +129,10 @@ for line in lines:
       nextScanner.append((x,y,z))
 S.append(nextScanner)
 
-B = S[0]
-
 def findPos(BOARD, bacons):
   B = []
   for b in bacons:
-    rots = rotations(b)
+    rots = getrotations(b)
     B.append(rots)
   
   maxMatch = -10e9
@@ -74,7 +141,7 @@ def findPos(BOARD, bacons):
   offyfinal = None
   offzfinal = None
 
-  for i in range(24):
+  for i in range(len(B[0])):
     for baconinboard in BOARD:
       for bi in range(len(B)):
         #print("Maxing", baconinboard, "with", B[bi][i])
@@ -103,55 +170,63 @@ def findPos(BOARD, bacons):
           offxfinal = offx
           offyfinal = offy
           offzfinal = offz
-          
-    
-  print("Max Match", maxMatch)
-  assert(maxMatch >= 12)
-  print("Matched", maxMatch)
-  MACHED = []
+
+  #assert(maxMatch >= 12)
+  print("Matching", maxMatch)
+  if maxMatch < 12:
+    return None
+  
+  TRANSFORMED = []
   for bi2 in range(len(B)):
     x = B[bi2][maxMatchi][0] + offxfinal
     y = B[bi2][maxMatchi][1] + offyfinal
     z = B[bi2][maxMatchi][2] + offzfinal
-    MACHED.append((x,y,z))
+    TRANSFORMED.append((x,y,z))
 
-  print("Relative", offxfinal, offyfinal, offzfinal)
-  return MACHED
+  #print("Relative", maxMatchi, offxfinal, offyfinal, offzfinal)
+  return TRANSFORMED
 
-rots = rotations((1,2,3))
-tocheck = [(1,2,3),
-           (1,-2,-3),
-           (1,3,-2),
-           (1,-3,2),
-           
-           (1,2,3),
-           (-1,2,-3),
-           (-3,2,1),
-           (3,2,-1),
-           
-           (1,2,3),
-           (-1,-2, 3),
-           (2,-1, 3),
-           (-2,1, 3)
-           
-           ]
-for check in tocheck:
-  if check not in rots:
-    print("not in", check)
-    assert(0)
+B = S[0]
 
-print(B)
-print("Len", len(B))
-for i in range(1, len(S)):
-  newPos = findPos(S[0], S[i])
-  npcount = 0
-  for np in newPos:
-    if np not in B:
-      B.append(np)
-    else:
-      print("Already matched", np)
-      npcount += 1
-  print("Count", npcount, len(newPos)-npcount)
-  print("Len", len(B))
+TRULE = {}
+MATCHED = [0]
 
-print("Part1", len(B))
+while(len(MATCHED) != 4):
+  for i in range(len(S)):
+    for j in range(len(S)):
+      if j != i:
+        print("Checking", i, j)
+        if i in MATCHED and j not in MATCHED:
+          TRANS = findPos(S[i], S[j])
+          if TRANS != None:
+            S[j] = TRANS
+            MATCHED.append(j)
+            for t in TRANS:
+              if t not in B:
+                B.append(t)
+            print("Sensor", i, "matches", j, MATCHED)
+
+
+print(MATCHED)
+print("Part1", len(B))  
+
+# while (len(MATCHED) != 5):
+#   for i in range(len(S)):
+#     for j in range(i+1, len(S)):
+#       print("Checking", i, j)
+#       if i != j:
+#         newPos, matchi, offx, offy, offz = findPos(S[i], S[j])
+#         if newPos != None:
+#           if i in MATCHED:
+#             if j not in MATCHED:
+#               print("Sensor", i, "matches", j)
+#               #print("S[j]", S[j])
+#               #print("newPos", newPos)
+#               S[j] = newPos
+#               MATCHED.append(j)
+#               for np in newPos:
+#                 if np not in B:
+#                   B.append(np)
+
+# print(MATCHED)
+# print("Part1", len(B))
---
+++ day19/day19_2.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python3
+import copy
+from os import device_encoding
+from typing import DefaultDict, List
+import sys
+from collections import defaultdict
+import heapq
+
+lines = open(sys.argv[1] if len(sys.argv) > 1 else "day19.dat", "r").read().splitlines()
+
+lines2= open("res.dat", "r").read().splitlines()
+RES = []
+for l in lines2:
+  RES.append(tuple(int(x) for x  in l.split(",")))
+
+
+def rotationangle(bacon, pitch, roll, yaw):
+  cosa = [1, 0, -1, 0][yaw] # cos 0, 90, 180, 270 = 1, 0, -1, 0
+  sina = [0, 1, 0, -1][yaw] # cos 0, 90, 180, 270 = 0, 1, 0, -1
+  
+  cosb = [1, 0, -1, 0][pitch] # cos 0, 90, 180, 270 = 1, 0, -1, 0
+  sinb = [0, 1, 0, -1][pitch] # cos 0, 90, 180, 270 = 0, 1, 0, -1
+  
+  cosc = [1, 0, -1, 0][roll] # cos 0, 90, 180, 270 = 1, 0, -1, 0
+  sinc = [0, 1, 0, -1][roll] # cos 0, 90, 180, 270 = 0, 1, 0, -1
+  
+  Axx = cosa*cosb
+  Axy = cosa*sinb*sinc - sina*cosc
+  Axz = cosa*sinb*cosc + sina*sinc
+
+  Ayx = sina*cosb
+  Ayy = sina*sinb*sinc + cosa*cosc
+  Ayz = sina*sinb*cosc - cosa*sinc
+
+  Azx = -sinb
+  Azy = cosb*sinc
+  Azz = cosb*cosc
+  
+  px,py,pz = bacon
+  x = Axx*px + Axy*py + Axz*pz
+  y = Ayx*px + Ayy*py + Ayz*pz
+  z = Azx*px + Azy*py + Azz*pz
+  
+  return (x,y,z)
+  
+  
+def getRotations(bacon):
+  return getRotations_old(bacon)
+
+def getRotations_old(bacon):
+  ret = []
+  for pitch in range(4):
+    for roll in range(4):
+      for yaw in range(4):
+        ret.append(rotationangle(bacon, pitch, roll, yaw))
+
+  return ret
+
+def getRotations_new(bacon):
+  ret = []
+  for ax in [1, -1]:
+    for ay in [1, -1]:
+      for az in [1, -1]:
+        x = bacon[0] * ax
+        y = bacon[1] * ay
+        z = bacon[2] * az
+        
+        ret.append((x,y,z))
+    
+  ret2 = []
+  for r in ret:
+    x,y,z = r
+    ret2.append((x,y,z))
+    ret2.append((y,z, x))
+    ret2.append((z, x, y))
+
+  assert(len(set(ret2))==64)
+  return ret2
+
+def scannersWithRotations(S):
+  ret = []
+  
+  for s in S:
+    tmp = []
+    for b in s:
+      tmp.append(getRotations(b))
+    ret.append(tmp)
+  
+  return ret
+
+def parseScanners(lines):
+  S = []
+  tmp = []
+  for line in lines:
+    if "---" in line:
+      print(line)
+    elif line == "":
+      S.append(tmp)
+      print("Len", len(tmp))
+      tmp = []
+    else:
+      x,y,z = [int(x) for x in line.split(",")]
+      tmp.append((x,y,z))
+  
+  if len(tmp) != 0:
+    S.append(tmp)
+    print("Len", len(tmp))
+  
+  return S
+
+def move(overlap, ri, ref):
+  ret = []
+  for p in overlap:
+    assert(len(p)==24)
+    assert(len(ref)==3)
+    x = p[ri][0]-ref[0]
+    y = p[ri][1]-ref[1]
+    z = p[ri][2]-ref[2]
+    ret.append((x,y,z))
+  return ret
+
+
+def getRotPoints(points, rotindex):
+  ret = []
+  for p in points:
+    ret.append(p[rotindex])
+  return ret
+
+def translate(points, translation):
+  ret = []
+  for p in points:
+    x = p[0] - translation[0]
+    y = p[1] - translation[1]
+    z = p[2] - translation[2]
+    ret.append((x,y,z))
+  return ret
+
+def foundOverlap(ref, overlap):
+  assert(len(overlap[0])==64)
+  for rotindex in range(len(overlap[0])):
+    rotpoints = getRotPoints(overlap, rotindex)
+    for r1 in ref:
+      for r2 in rotpoints:
+        offx = r2[0] - r1[0]
+        offy = r2[1] - r1[1]
+        offz = r2[2] - r1[2]
+        trans = translate(rotpoints, (offx, offy, offz))
+        match = 0
+        for t in trans:
+          if t in ref:
+            match += 1
+        if match >= 12:
+          return trans
+    
+  return None
+
+S = parseScanners(lines)
+S = scannersWithRotations(S)
+
+FOUNDS = [0]
+NOTFOUNDS = [x for x in range(1, len(S))]
+BOARD = getRotPoints(S[0], 0)
+TRANSFORMED = {0: getRotPoints(S[0], 0)}
+
+print("Found", FOUNDS, "notfound", NOTFOUNDS)
+I = set()
+
+while len(NOTFOUNDS) != 0:
+  print("Still not found", NOTFOUNDS)
+  NEWFOUNDS = FOUNDS.copy()
+  NEWNOTFOUNDS = NOTFOUNDS.copy()
+  found = False
+  for f in FOUNDS:
+    for nf in NOTFOUNDS:
+      if (f,nf) in I:
+        break
+      print("Checking", f, nf)
+      transformed = foundOverlap(TRANSFORMED[f], S[nf])
+      if transformed == None:
+        #I.add((f,nf))
+        pass
+      else:
+        NEWFOUNDS.insert(0, nf)
+        NEWNOTFOUNDS.remove(nf)
+        oldlen = len(BOARD)
+        for t in transformed:
+          if t not in BOARD:
+            BOARD.append(t)
+        TRANSFORMED[nf] = transformed
+        print("FOUND", f, nf)
+        print("Oldlen", oldlen, "newlen", len(BOARD), len(transformed))
+        found = True
+        break
+    if found:
+      break
+  FOUNDS = NEWFOUNDS
+  NOTFOUNDS = NEWNOTFOUNDS
+  # if len(FOUNDS) == 4:
+  #   print("LENTRANS", len(TRANSFORMED))
+  #   print("LENBOARD", len(BOARD))
+  #   print(TRANSFORMED)
+  #   print("FOUND")
+  #   founded = 0
+  #   for b in BOARD:
+  #     if b in RES:
+  #       print(b)
+  #       founded += 1
+  #   print("FOUNDED", founded)
+  #   print("NOTFOUND")
+  #   notfound = 0
+  #   for b in BOARD:
+  #     if b not in RES:
+  #       notfound += 1
+  #       print(b)
+  #   print("NOT FOUND", notfound)
+  #   for i in range(24):
+  #     rot = move(S[2], i, (-1105,1205,-1229))
+  #     print("ROTATE", i)
+  #     for j, r in enumerate(rot):
+  #       print(r, S[2][j][i])
+    #exit()
+    
+print("Part1:", len(BOARD))
