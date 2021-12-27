f643ae6c8896c99f89f4a9fc04c94723b5219ec3
Mariano Cerdeiro <mcerdeiro@gmail.com>
2021-12-27 14:07:08 +0100

    minimal cleaning

--- day22/day22_4.py
+++ day22/day22_4.py
@@ -8,13 +8,6 @@ import heapq
 
 lines = open(sys.argv[1] if len(sys.argv) > 1 else "day22.dat", "r").read().splitlines()
 
-def currentlyOn(R):
-  ans1 = 0
-  for r in R:
-    if R[r] == 1:
-      ans1 += 1
-  return ans1
-
 def getCountCubes(p):
   x = 0
   y = 0
@@ -23,26 +16,8 @@ def getCountCubes(p):
   y = abs(p[1][0]-p[1][1])+1
   z = abs(p[2][0]-p[2][1])+1
   return x*y*z
-
-D = []
-for line in lines:
-  if "#" in line:
-    continue
-  on = False
-  if "on" in line:
-    on = True
-  
-  x = line.split("x=")[1].split(",y")[0]
-  y = line.split("x=")[1].split(",y=")[1].split(",z=")[0]
-  z = line.split("x=")[1].split(",y=")[1].split(",z=")[1]
-  x1, x2 = [int(a) for a in x.split("..")]
-  y1, y2 = [int(b) for b in y.split("..")]
-  z1, z2 = [int(c) for c in z.split("..")]
-
-  p = ((x1,x2), (y1, y2), (z1, z2))
-  D.append((p, on))
-    
-def sameRange(x, x1):
+   
+def overlap(x, x1):
   if x1[0] <= x[0] <= x1[1]:
     return True
   if x1[0] <= x[1] <= x1[1]:
@@ -55,16 +30,16 @@ def sameRange(x, x1):
   return False
 
 def hasIntersection(p1, p2):
-  if sameRange(p1[0], p2[0]):
-    if sameRange(p1[1], p2[1]):
-      if sameRange(p1[2], p2[2]):
+  if overlap(p1[0], p2[0]):
+    if overlap(p1[1], p2[1]):
+      if overlap(p1[2], p2[2]):
         return True
   return False
 
-def getInside(r1, r2):
+def getInsideForOneCoordinate(r1, r2):
   return (max(r1[0], r2[0]), min(r1[1], r2[1]))
 
-def getOutside(r1, r2):
+def getOutsideForOneCoordinate(r1, r2):
   outside = []
   if r1[0] < r2[0]:
     outside.append((r1[0], r2[0]-1))
@@ -73,38 +48,32 @@ def getOutside(r1, r2):
   
   return outside
 
-def getOutsides(r1, r2):
-  outx = getOutside(r1[0], r2[0])
-  outy = getOutside(r1[1], r2[1])
-  outz = getOutside(r1[2], r2[2])
+def getOutside(r1, r2):
+  outx = getOutsideForOneCoordinate(r1[0], r2[0])
+  outy = getOutsideForOneCoordinate(r1[1], r2[1])
+  outz = getOutsideForOneCoordinate(r1[2], r2[2])
   
   assert(len(outx) < 3)
   assert(len(outy) < 3)
   assert(len(outz) < 3)
   return outx, outy, outz
 
-def getInsides(r1, r2):
-  inx = getInside(r1[0], r2[0])
-  iny = getInside(r1[1], r2[1])
-  inz = getInside(r1[2], r2[2])
+def getInside(r1, r2):
+  inx = getInsideForOneCoordinate(r1[0], r2[0])
+  iny = getInsideForOneCoordinate(r1[1], r2[1])
+  inz = getInsideForOneCoordinate(r1[2], r2[2])
   
   return inx, iny, inz
 
 def removeOffs(group, OFF):
-  print("Processing group", group)
   result = [group]
-  inoff = False
   for off in OFF:
     nexttoprocess = []  
-    inoff = True
     for process in result:
       if hasIntersection(process[0], off[0]):
         nexttoprocess += getOutsideGroups(process[0], off[0])
       else:
-        print("Nointersection", process)
-        print("Before", nexttoprocess)
         nexttoprocess.append(process)
-        print("After", nexttoprocess)
     result = nexttoprocess
 
   return result
@@ -127,56 +96,25 @@ def countOnOffIntersections(groups:list):
           offswithintersection.append(off[1])
     if len(offswithintersection) > 0:
       withoutoffs = removeOffs(group, offswithintersection)
-      print("*******************Adding here1", withoutoffs)
       groupswithoutoff += withoutoffs
     else:
-      print("*******************Adding here2", group)
       groupswithoutoff.append(group)
-      
-    # # only for on groups
-    # if group[1] == True:
-    #   print("Processing group", i, group)
-    #   toprocess = [group]
-    #   nexttoprocess = []
-    #   inoff = False
-    #   for off in OFF:
-    #     if i < off[0]:
-    #       inoff = True
-    #       for process in toprocess:
-    #         #off apply to this group
-    #         if hasIntersection(process[0], off[1][0]):
-    #           print("Has intersection with off", group[0], "off", off[1])
-    #           nexttoprocess += getOutsideGroups(process[0], off[1][0])
-    #     toprocess = nexttoprocess
-    #   if inoff == False:
-    #     toprocess.append(group)
-    #   groupswithoutoff += toprocess
-    #   print("Result", toprocess)
-    # else:
-    #   #print("ignore off", group)
-    #   pass
-
-  print("After off")
-  for gwo in groupswithoutoff:
-    print("**", gwo)
+          
   
   return countOnIntersections(groupswithoutoff)
 
 def getOutsideGroups(range, outrange):
   newtoadd = []
   outx, outy, outz = [None, None, None]
-  outx, outy, outz = getOutsides(range, outrange)
+  outx, outy, outz = getOutside(range, outrange)
   taon = True
-  # else:
-  #   outx, outy, outz = getInsides(tarange, orange)
-  # #print("Outsides x", outx, "y", outy, "z", outz)
   
   xrange = None
   if len(outx) > 0:
-    xrange = getOutside(range[0], outx[0])
+    xrange = getOutsideForOneCoordinate(range[0], outx[0])
     assert(len(xrange) == 1)
     if len(outx) == 2:
-      xrange = getOutside(xrange[0], outx[1])
+      xrange = getOutsideForOneCoordinate(xrange[0], outx[1])
     assert(len(xrange) == 1)
     #print("xrange", xrange)
     xrange = xrange[0]
@@ -185,11 +123,11 @@ def getOutsideGroups(range, outrange):
   
   yrange = None
   if len(outy) > 0:
-    yrange = getOutside(range[1], outy[0])
+    yrange = getOutsideForOneCoordinate(range[1], outy[0])
     #print("getOutside from", range[1], outy[0], "returned", yrange)
     assert(len(yrange) == 1)
     if len(outy) == 2:
-      yrange = getOutside(yrange[0], outy[1])
+      yrange = getOutsideForOneCoordinate(yrange[0], outy[1])
     assert(len(yrange) == 1)
     #print("yrange", yrange)
     yrange = yrange[0]
@@ -198,10 +136,10 @@ def getOutsideGroups(range, outrange):
   
   zrange = None
   if len(outz) > 0:
-    zrange = getOutside(range[2], outz[0])
+    zrange = getOutsideForOneCoordinate(range[2], outz[0])
     assert(len(zrange) == 1)
     if len(outz) == 2:
-      zrange = getOutside(zrange[0], outz[1])
+      zrange = getOutsideForOneCoordinate(zrange[0], outz[1])
     assert(len(zrange) == 1)
     #print("zrange", zrange)
     zrange = zrange[0]
@@ -268,42 +206,26 @@ def countOnIntersections(groups):
   
   return result
 
-ans2 = 0
-OLD = []
 
-@DeprecationWarning
-def remove(OLD, toremove):
-  print("Removing", toremove)
-  count = 0
-  OLDINSIDE = []
-  for old in OLD:
-    OLDINSIDE.append(getInsides(toremove, old[0]))
-    
-  print("Removing", toremove, OLDINSIDE)
-  #exit()
+D = []
+for line in lines:
+  if "#" in line:
+    continue
+  on = False
+  if "on" in line:
+    on = True
+  
+  x = line.split("x=")[1].split(",y")[0]
+  y = line.split("x=")[1].split(",y=")[1].split(",z=")[0]
+  z = line.split("x=")[1].split(",y=")[1].split(",z=")[1]
+  x1, x2 = [int(a) for a in x.split("..")]
+  y1, y2 = [int(b) for b in y.split("..")]
+  z1, z2 = [int(c) for c in z.split("..")]
 
-R = {}
-ON = []
-ans2 = 0
-new = 0
-for d in D:
-  on = d[1]
-  x1, x2 = d[0][0]
-  y1, y2 = d[0][1]
-  z1, z2 = d[0][2]
-  # for x in range(x1, x2+1):
-  #   for y in range(y1, y2+1):
-  #     for z in range(z1, z2+1):
-  #       if on:
-  #         R[(x,y,z)] = 1
-  #       else:
-  #         R[(x,y,z)] = 0
-          
-  # new = currentlyOn(R)
-
+  p = ((x1,x2), (y1, y2), (z1, z2))
+  D.append((p, on))
+  
 ans2 = countOnOffIntersections(D)
-print("countOnOffIntersections(D)", ans2)
-print("Manual", new)
-assert(new == ans2)
+print("Part2:", ans2)
 
     
